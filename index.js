62985ddb3873a1ebed1040b39807389d8e174416
Nathianny Pereira <nathianny_pereira26@hotmail.com>
2023-02-02 12:19:59 -0300

    botão

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Executei o Javascript ...");
+
+function calcularIdade(){
+var ano_nascimento = prompt("Em que ano você nasceu?");
+
+var idade = 2023 - ano_nascimento;
+
+alert(idade);
+}
